4860b65684c43c32c3dd8b55f849789ba317e974
unknown <eservetti2018@gmail.com>
2024-09-13 02:00:59 -0300

    Agregada funcion para enviar solicitud y ver lista de amigos

---
+++ src/components/main/friendsList.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import useFriends from "../../hooks/useFriends";
+
+function FriendsList({setFriendInfo}) {
+  //Manejo de lista de amigos
+  const { friends, loading } = useFriends();
+
+  const [onFriend, setOnFriend ] = useState(null)
+
+  function handleClick(index, friend) {
+    setOnFriend(index)
+    setFriendInfo(friend)
+  }
+  return (
+    <section className="friends-list">
+      {loading ? (
+        <span>Loading...</span>
+      ) : friends.length === 0 ? (
+        <span>You don't have any friends yet, invite them and share your books!</span>
+      ) : (
+        friends.map((friend, index) => (
+          <div className="friend" key={index} onClick={() => handleClick(index, friend)} style={{boxShadow: onFriend === index ?" 0 0 5px 0 white" : "none"}}>
+            <img className="friend-img" src={friend.photo} alt="user photo" />
+            <section className="friend-info">
+              <span>{friend.name}</span>
+              <span className="friends-email">{friend.email}</span>
+            </section>
+          </div>
+        ))
+      )}
+    </section>
+  );
+}
+
+export default FriendsList;
---
+++ src/hooks/useFriends.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { path } from "../path";
+import { useEffect, useState } from "react";
+
+function useFriends() {
+  const [friends, setFriends] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`${path}/api/users/friends`, { withCredentials: true })
+      .then((res) => {
+        const response = res.data;
+        if (response.statusCode === 200) {
+          setFriends(response.message);
+          setLoading(false)
+        }
+        else if(response.statusCode === 404) {
+            setFriends([]);
+            setLoading(false)
+        } else {
+            location.reload()
+        }
+      });
+  }, []);
+  return { friends, loading };
+}
+
+export default useFriends;
